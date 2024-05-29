c2e10f74d7c90861ba1e56ec576a09f15a251ab5
Nicolò <nico.2699@icloud.com>
2024-05-29 13:24:20 +0200

    add : input-valori

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('sono collegato');
+//console.log('sono collegato');
+
+//Input utente
+const inputTipoLavoro = document.getElementById('tipo_lavoro');
+const inputCodicePromozionale = document.getElementById('codice_promozionale');
+const inputNome = document.getElementById('Nome');
+const inputCognome = document.getElementById('Cognome');
+const inputEmail = document.getElementById('Email');
+
+
+// Valori per calcolo
+var boolTipLavoro = false;
+const prezzoBackend = 20.50;
+const prezzoFrontend = 15.30;
+const prezzoAnalisi = 33.60;
+//ore di lavoro 6
+
+
+
+
+
+
+
